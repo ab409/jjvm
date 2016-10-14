687df1f14ba7ecd3b11abb5f3bdb48538d195970
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-10-14 15:10:50 +0800

    or instruction

---
+++ src/main/java/me/ygy/jjvm/instructions/math/IOr.java
@@ -0,0 +1,20 @@
+package me.ygy.jjvm.instructions.math;
+
+import me.ygy.jjvm.instructions.base.NoOperandsInstruction;
+import me.ygy.jjvm.rtda.Frame;
+import me.ygy.jjvm.rtda.OperandStack;
+
+/**
+ * Created by guangyuanyu on 2016/10/14.
+ */
+public class IOr extends NoOperandsInstruction {
+
+    @Override
+    public void execute(Frame frame) {
+        OperandStack stack = frame.getOperandStack();
+        int v2 = stack.popInt();
+        int v1 = stack.popInt();
+        int result = v1 | v2;
+        stack.pushInt(result);
+    }
+}
