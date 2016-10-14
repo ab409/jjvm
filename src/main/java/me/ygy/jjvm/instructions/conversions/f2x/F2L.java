51b024917ad786d262e3f6ffde210149cd773cef
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-10-14 15:35:50 +0800

    f2x instruction

---
+++ src/main/java/me/ygy/jjvm/instructions/conversions/f2x/F2L.java
@@ -0,0 +1,17 @@
+package me.ygy.jjvm.instructions.conversions.f2x;
+
+import me.ygy.jjvm.instructions.base.NoOperandsInstruction;
+import me.ygy.jjvm.rtda.Frame;
+import me.ygy.jjvm.rtda.OperandStack;
+
+/**
+ * Created by guangyuanyu on 2016/10/14.
+ */
+public class F2L extends NoOperandsInstruction {
+
+    @Override
+    public void execute(Frame frame) {
+        OperandStack stack = frame.getOperandStack();
+        Float v = stack.popFloat();
+        stack.pushLong(v.longValue());
+    }}
