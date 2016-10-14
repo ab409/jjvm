421df2e691aac9d1d3f08bbfbacab9ceb666b746
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-10-14 15:08:56 +0800

    neg instruction

---
+++ src/main/java/me/ygy/jjvm/instructions/math/DNeg.java
@@ -0,0 +1,18 @@
+package me.ygy.jjvm.instructions.math;
+
+import me.ygy.jjvm.instructions.base.NoOperandsInstruction;
+import me.ygy.jjvm.rtda.Frame;
+import me.ygy.jjvm.rtda.OperandStack;
+
+/**
+ * Created by guangyuanyu on 2016/10/14.
+ */
+public class DNeg extends NoOperandsInstruction {
+
+    @Override
+    public void execute(Frame frame) {
+        OperandStack stack = frame.getOperandStack();
+        double v = stack.popDouble();
+        stack.pushDouble(-v);
+    }
+}
