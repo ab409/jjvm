ff340ebb4e1aba0010d8e1c7143180f9dcd7b857
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-10-14 15:33:03 +0800

    d2x instruction

---
+++ src/main/java/me/ygy/jjvm/instructions/conversions/d2x/D2L.java
@@ -0,0 +1,18 @@
+package me.ygy.jjvm.instructions.conversions.d2x;
+
+import me.ygy.jjvm.instructions.base.NoOperandsInstruction;
+import me.ygy.jjvm.rtda.Frame;
+import me.ygy.jjvm.rtda.OperandStack;
+
+/**
+ * Created by guangyuanyu on 2016/10/14.
+ */
+public class D2L extends NoOperandsInstruction {
+
+    @Override
+    public void execute(Frame frame) {
+        OperandStack stack = frame.getOperandStack();
+        Double d = stack.popDouble();
+        stack.pushLong(d.longValue());
+    }
+}
