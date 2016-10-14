4271acf3f92b0f6a7b43adc5773957794ef8ef57
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-10-14 16:14:47 +0800

    ifIcmp instruction

---
+++ src/main/java/me/ygy/jjvm/instructions/comparisons/ificmp/IfICmpEq.java
@@ -0,0 +1,21 @@
+package me.ygy.jjvm.instructions.comparisons.ificmp;
+
+import me.ygy.jjvm.instructions.base.BranchInstruction;
+import me.ygy.jjvm.rtda.Frame;
+import me.ygy.jjvm.rtda.OperandStack;
+
+/**
+ * Created by guangyuanyu on 2016/10/14.
+ */
+public class IfICmpEq extends BranchInstruction{
+
+    @Override
+    public void execute(Frame frame) {
+        OperandStack stack = frame.getOperandStack();
+        int v2 = stack.popInt();
+        int v1 = stack.popInt();
+        if (v1 == v2) {
+            super.branch(frame, this.offset);
+        }
+    }
+}
