fe821e66211ebbaf142f10e156f80a35ca069ad6
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-10-14 15:21:55 +0800

    ldiv instruction

---
+++ src/main/java/me/ygy/jjvm/instructions/math/LDiv.java
@@ -0,0 +1,22 @@
+package me.ygy.jjvm.instructions.math;
+
+import me.ygy.jjvm.instructions.base.NoOperandsInstruction;
+import me.ygy.jjvm.rtda.Frame;
+import me.ygy.jjvm.rtda.OperandStack;
+
+/**
+ * Created by guangyuanyu on 2016/10/14.
+ */
+public class LDiv extends NoOperandsInstruction {
+
+    @Override
+    public void execute(Frame frame) {
+        OperandStack stack = frame.getOperandStack();
+        long v2 = stack.popLong();
+        long v1 = stack.popLong();
+        if (v2 == 0)
+            throw new ArithmeticException("long div by zero");
+        long result = v1 / v2;
+        stack.pushLong(result);
+    }
+}
