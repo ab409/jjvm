b9d5605563f9c9fe0aa017762020777247245ef4
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-10-14 16:17:58 +0800

    ifacmp instruction

---
+++ src/main/java/me/ygy/jjvm/instructions/comparisons/ifacmp/IfACmpNe.java
@@ -0,0 +1,21 @@
+package me.ygy.jjvm.instructions.comparisons.ifacmp;
+
+import me.ygy.jjvm.instructions.base.BranchInstruction;
+import me.ygy.jjvm.rtda.Frame;
+import me.ygy.jjvm.rtda.OperandStack;
+
+/**
+ * Created by guangyuanyu on 2016/10/14.
+ */
+public class IfACmpNe extends BranchInstruction {
+
+    @Override
+    public void execute(Frame frame) {
+        OperandStack stack = frame.getOperandStack();
+        Object ref2 = stack.popRef();
+        Object ref1 = stack.popRef();
+        if (ref1 != ref2) {
+            super.branch(frame, this.offset);
+        }
+    }
+}
