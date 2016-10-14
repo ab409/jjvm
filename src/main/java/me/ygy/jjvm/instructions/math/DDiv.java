080cbf3d6093186ffdf861694d7aeaf040010404
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-10-14 15:02:30 +0800

    div instruction

---
+++ src/main/java/me/ygy/jjvm/instructions/math/DDiv.java
@@ -0,0 +1,20 @@
+package me.ygy.jjvm.instructions.math;
+
+import me.ygy.jjvm.instructions.base.NoOperandsInstruction;
+import me.ygy.jjvm.rtda.Frame;
+import me.ygy.jjvm.rtda.OperandStack;
+
+/**
+ * Created by guangyuanyu on 2016/10/14.
+ */
+public class DDiv extends NoOperandsInstruction {
+
+    @Override
+    public void execute(Frame frame) {
+        OperandStack stack = frame.getOperandStack();
+        double v2 = stack.popDouble();
+        double v1 = stack.popDouble();
+        double result = v1 / v2;
+        stack.pushDouble(result);
+    }
+}
