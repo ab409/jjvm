817e9138df8e4fc0865833cd83bce65491fdcf58
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-10-14 15:40:42 +0800

    i2x instruction

---
+++ src/main/java/me/ygy/jjvm/instructions/conversions/i2x/I2L.java
@@ -0,0 +1,18 @@
+package me.ygy.jjvm.instructions.conversions.i2x;
+
+import me.ygy.jjvm.instructions.base.NoOperandsInstruction;
+import me.ygy.jjvm.rtda.Frame;
+import me.ygy.jjvm.rtda.OperandStack;
+
+/**
+ * Created by guangyuanyu on 2016/10/14.
+ */
+public class I2L extends NoOperandsInstruction {
+
+    @Override
+    public void execute(Frame frame) {
+        OperandStack stack = frame.getOperandStack();
+        Integer i = stack.popInt();
+        stack.pushLong(i.longValue());
+    }
+}
