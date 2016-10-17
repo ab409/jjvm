c66754a99ad6fda2fb7ebd09c86936606fd8f2bd
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-10-17 11:11:40 +0800

    xa store instructions init

---
+++ src/main/java/me/ygy/jjvm/instructions/stores/xa/AAStore.java
@@ -0,0 +1,9 @@
+package me.ygy.jjvm.instructions.stores.xa;
+
+import me.ygy.jjvm.instructions.base.NoOperandsInstruction;
+
+/**
+ * Created by guangyuanyu on 2016/10/17.
+ */
+public class AAStore extends NoOperandsInstruction {
+}
