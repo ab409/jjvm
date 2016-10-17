721061312746a183fbe1cd6193e73ad7b26eb3d5
Yu guangyuan <guangyuanyu@sohu-inc.com>
2016-10-17 11:55:16 +0800

    if null if not null instruction

---
+++ src/main/java/me/ygy/jjvm/instructions/extended/IfNull.java
@@ -0,0 +1,18 @@
+package me.ygy.jjvm.instructions.extended;
+
+import me.ygy.jjvm.instructions.base.BranchInstruction;
+import me.ygy.jjvm.rtda.Frame;
+
+/**
+ * Created by guangyuanyu on 2016/10/17.
+ */
+public class IfNull extends BranchInstruction {
+
+    @Override
+    public void execute(Frame frame) {
+        Object ref = frame.getOperandStack().popRef();
+        if (ref == null) {
+            branch(frame, this.offset);
+        }
+    }
+}
